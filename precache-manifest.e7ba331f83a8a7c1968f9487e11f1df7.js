acee340c9083a2cff89bf022bf6567230932ff02
vatsalgp <vatsalgupta280@gmail.com>
2020-02-01 21:52:18 +0530

    Updates

---
+++ precache-manifest.e7ba331f83a8a7c1968f9487e11f1df7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2417a19a3898e9e6c83ec04253c10d0",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "fbdf6b710af150d7881a",
+    "url": "/youtube/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "9a4efac3912bf2778bb4",
+    "url": "/youtube/static/js/2.5b78d634.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/youtube/static/js/2.5b78d634.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbdf6b710af150d7881a",
+    "url": "/youtube/static/js/main.92df6920.chunk.js"
+  },
+  {
+    "revision": "948b31b39ab85a3d5439",
+    "url": "/youtube/static/js/runtime-main.13c75821.js"
+  }
+]);
