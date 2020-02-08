a4fc3e800463c24bf350463fd79cd764683c7af0
vatsalgp <vatsalgupta280@gmail.com>
2020-02-08 20:58:44 +0530

    Added Fullscreen

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,24 +1,26 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-  if (!video) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div>
-      <div className="ui embed">
-        <iframe
-          src={"https://www.youtube.com/embed/" + video.id.videoId}
-          title="Video Player"
-        />
-      </div>
-      <div className="ui segment">
-        <h4 className="ui header">{video.snippet.title}</h4>
+    if (!video) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div>
+            <div className="ui embed">
+                <iframe
+                    src={"https://www.youtube.com/embed/" + video.id.videoId}
+                    title="Video Player"
+                    frameborder="0"
+                    allowfullscreen
+                />
+            </div>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
 
-        <div className="description">{video.snippet.description}</div>
-      </div>
-    </div>
-  );
+                <div className="description">{video.snippet.description}</div>
+            </div>
+        </div>
+    );
 };
 
 export default VideoDetail;
